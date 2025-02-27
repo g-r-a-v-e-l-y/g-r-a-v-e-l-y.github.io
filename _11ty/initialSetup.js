6a9c56f98a623a453050e389723b03e04d658527
Grant Stavely <grant@grantstavely.com>
2025-02-27 15:56:44 -0800

    trying to learn how to have a second shorter excerpt

--- _11ty/initialSetup.js
+++ _11ty/initialSetup.js
@@ -32,6 +32,27 @@ module.exports = function (eleventyConfig) {
       .use(asidePlugin),
   );
 
+  eleventyConfig.setFrontMatterParsingOptions({
+      excerpt: (file) => {
+
+          // I use https://www.npmjs.com/package/remove-markdown here,
+          // but you can bring your own de-markdownifier.
+          let plaintext = removeMarkdown(file.content).trim();
+
+          // End the description at a period (inclusive) or newline (not)
+          // somewhere around the lenghtish mark.
+          let lenghtish = 60;
+          let dot = plaintext.indexOf(".", lenghtish) + 1;
+          let newline = plaintext.indexOf("\n", lenghtish);
+
+          // Avoid substringing to the empty string
+          if (dot === -1) dot = plaintext.length;
+          if (newline === -1) newline = plaintext.length;
+
+          file.excerpt = plaintext.substring(0, Math.min(dot, newline));
+      },
+  });
+
   eleventyConfig.addPassthroughCopy({
     [ASSETS_FOLDER]: './',
     [SCRIPTS_FOLDER]: './js',
